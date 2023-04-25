0a4143e41ba16f82a7c71ab40cb8c24adb1fdb2b
madli-mai j6e <madlimai.joe@gmail.com>
2023-04-25 08:50:10 +0300

    Sql fix

--- php_oop/TestScript.php
+++ php_oop/TestScript.php
@@ -4,13 +4,12 @@ require_once('Database.php');
 
 $db = new Database;
 $result = $db->execute("
-        SELECT pn, first, last 
-        FROM patient;");
-$patient_numbers= $result->fetch_all(MYSQLI_ASSOC);
+        SELECT pn FROM patient;");
+$patient_numbers = $result->fetch_all(MYSQLI_ASSOC);
 asort($patient_numbers);
-foreach($patient_numbers as $pn){
+foreach ($patient_numbers as $pn) {
     $patient = new Patient($pn["pn"]);
-    echo $patient -> validate(date('m-d-y'));
+    echo $patient->validate(date('m-d-y'));
 }
 
 $db->close();
